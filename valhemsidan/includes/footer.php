615e0e49b9ea5f6219c96670077cfec02eac8a37
vreveronz <vreveron83@gmail.com>
2015-03-07 14:22:14 +0100

    Added clarification on the footer

--- valhemsidan/includes/footer.php
+++ valhemsidan/includes/footer.php
@@ -4,6 +4,7 @@
             <li>Kontakt</li>
             <li>Personuppgifter (PUL)</li>
             <li>Om kakor</li>
+            <li>This is a study project with no relation to Valmyndigheten</li>
         </ul>
     </div>
     </footer>
